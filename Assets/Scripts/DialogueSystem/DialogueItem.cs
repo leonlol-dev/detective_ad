c3db92a3e6300d7e859a0d2e23a06fdcd05d2048
Leon <59918677+leonlol-dev@users.noreply.github.com>
2023-02-16 16:25:15 +0000

    dialogue system start

---
+++ Assets/Scripts/DialogueSystem/DialogueItem.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    public string[] sentences;
+    public AudioClip[] sentencesClip;
+    
+}
---
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> dialogueQueue = new Queue<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
